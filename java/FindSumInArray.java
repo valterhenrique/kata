d5819db8ec1faf05dfc5ffbb53ccf36cbbd8f67c
Valter Silva <valter.silva@anevis-solutions.com>
2017-01-20 22:27:04 +0100

    Link for the problem: https://www.careercup.com/question?id=6285101383024640

---
+++ java/FindSumInArray.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class FindSumInArray {
+
+  public static void main(String[] args){
+    int[] array = new int[]{1,-10,18,29,75,12,3};
+    int k = 15;
+    boolean wasFound = new FindSumInArray().isFound(array, k);
+
+    if (wasFound){
+      System.out.println("K was found in the array");
+    }else{
+      System.out.println("K was NOT found");
+    }
+  }
+
+  private boolean isFound(int[] array, int k){
+    for (int i = 0; i < array.length -1; i++){
+      for (int j = 1; j < array.length; j++){
+        if ((array[i]+array[j]) == k){
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+}
